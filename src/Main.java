a4fe535100f39f0b0be1cbb342a170eb27c0c4fd
Luebu <maximilian.luecker@hebk.de>
2020-08-26 10:08:40 +0200

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         Insertion sort = new Insertion();
-        int[] arr = {5, 3, 7, 9, 1};
+        int[] arr = {5,3453, 7, 9, 13};
         for (int i = 0; i < arr.length; i++)
             System.out.print(sort.insertionSort(arr)[i]+"   ");
     }
